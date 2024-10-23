bebd360daaca5ad9bd9644e79d9bc9b0db1776d0
Muavia-Shakeel <muaviashakeel70@gmail.com>
2024-10-23 17:10:43 +0500

    Project Structure (Logging & Exception

--- setup.py
+++ setup.py
@@ -3,26 +3,28 @@ from typing import List
 
 
 HYPEN = '-e .'
-def get_requirements(file_path:str)->List[str]:
-  """This function will return the list of requirments"""
-  requirements = []
-  with open(file_path) as file_obj: #temporary file object
-    requirements = file_obj.readlines()
-    [req.replace("\n", "") for req in requirements]
+
+def get_requirements(file_path: str) -> List[str]:
+    """This function will return the list of requirements"""
+    requirements = []
+    with open(file_path) as file_obj:
+        # Read all lines from the requirements.txt
+        requirements = file_obj.readlines()
+        # Strip newline characters and whitespace
+        requirements = [req.strip() for req in requirements]
+        
+        # Remove '-e .' if it's in the requirements
+        if HYPEN in requirements:
+            requirements.remove(HYPEN)
     
-    if HYPEN in requirements:
-      requirements.remove(HYPEN)
-      
     return requirements
 
 
-
-
 setup(
-  name= 'genericmlproject',
-  version='0.0.1',
-  author='Muavia',
-  author_email='muaviashakeel70@gmail.com',
-  packages=find_packages(),
-  install_requires= get_requirements('requirements.txt')
+    name='genericmlproject',
+    version='0.0.1',
+    author='Muavia',
+    author_email='muaviashakeel70@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
 )
--- src/logger.py
+++ src/logger.py
@@ -1,1 +1,24 @@
- 
+import logging
+import os
+from datetime import datetime
+
+# Creating log file name with timestamp format "MM_DD_YYYY_HH_MM_SS.log"
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# Defining the directory where logs will be stored
+logs_path = os.path.join(os.getcwd(), "logs")
+
+# Creating the logs directory if it doesn't exist
+os.makedirs(logs_path, exist_ok=True)
+
+# Defining the full path to the log file
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# Configuring logging with the specified settings
+logging.basicConfig(
+    filename=LOG_FILE_PATH,  # Logging file path
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",  # Log format
+    level=logging.INFO,  # Setting the logging level to INFO
+)
+
+
